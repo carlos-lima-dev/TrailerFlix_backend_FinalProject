4cc1203425d2c6e20ef24071e2fb26bbcb2ac47d
Carlos Lima <150730209+carlos-lima-dev@users.noreply.github.com>
2024-04-19 22:23:45 +0100

    Update constants.js

--- backend final project/front-end/src/constants/constants.js
+++ backend final project/front-end/src/constants/constants.js
@@ -1,5 +1,5 @@
-export const BLOG_API_REGISTER = "http://localhost:3000/auth/register";
-export const BLOG_API_LOGIN = "http://localhost:3000/auth/login";
-export const BLOG_API_LOGIN_ADMIN = "http://localhost:3000/auth/admin";
-export const BLOG_API_FETCH_USERS = "http://localhost:3000/auth/users";
-export const BLOG_API_FETCH_MOVIES = "http://localhost:3000/api/movies";
+export const BLOG_API_REGISTER = "https://backend-build-api-11.onrender.com/auth/register";
+export const BLOG_API_LOGIN = "https://backend-build-api-11.onrender.com/auth/login";
+export const BLOG_API_LOGIN_ADMIN = "https://backend-build-api-11.onrender.com/auth/admin";
+export const BLOG_API_FETCH_USERS = "https://backend-build-api-11.onrender.com/auth/users";
+export const BLOG_API_FETCH_MOVIES = "https://backend-build-api-11.onrender.com/api/movies";
